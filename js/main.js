545c503cc2856e6425e1cc8bc742d6cfd3275f16
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-05-05 16:29:19 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active')
+});
+
+
+
 var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
